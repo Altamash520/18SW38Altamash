f95ce9345565ab192112e29a7d807774942d6b61
Altamash520 <49789166+Altamash520@users.noreply.github.com>
2019-04-23 20:00:08 +0500

    Add files via upload

---
+++ lab 3/lab 2/Lab2Task4.java
@@ -0,0 +1,14 @@
+class Lab2Task4{
+	public static void main(String args[]){
+			float r_circle=5.23f;
+			double diameter= 2*r_circle;
+			double circumference=2*3.142*r_circle;
+			double area= 3.142*r_circle*r_circle;
+
+			System.out.println("Radius Of Circle:"+r_circle);
+			System.out.println("Diameter: "+diameter);
+			System.out.println("Circumference= "+circumference);
+			System.out.println("Area="+area);
+
+	}
+}
